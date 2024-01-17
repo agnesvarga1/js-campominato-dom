9f26fdb10426778cac9b24868254062c862abfd1
agnesvarga1 <agikolyok@gmail.com>
2024-01-17 10:57:11 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+const mainHtml = document.querySelector("main");
+const gridHtml = document.querySelector("#grid");
+const playBtn = document.querySelector(".play-btn");
+const rstBtn = document.querySelector(".rst-btn");
+const selectHtml = document.querySelector("#diff-level");
+
+function startGame(n) {
+  let cellPerLine = n;
+  for (let i = 1; i <= cellPerLine ** 2; i++) {
+    let cellHtml = document.createElement("div");
+    const wCalced = `calc(100% / ${cellPerLine})`;
+    cellHtml.style.setProperty("width", wCalced);
+    cellHtml.classList.add("cell");
+    cellHtml.innerHTML = `<span>${i}</span>`;
+
+    cellHtml.addEventListener("click", function () {
+      this.classList.toggle("active");
+      let cellNum = this.querySelector("span").innerText;
+      console.log(cellNum);
+    });
+    gridHtml.append(cellHtml);
+  }
+}
+
+playBtn.addEventListener("click", () => {
+  let diffLevel = selectHtml.value;
+  mainHtml.classList.remove("noshow");
+  playBtn.classList.add("noshow");
+  rstBtn.classList.remove("noshow");
+  startGame(diffLevel);
+});
+
+rstBtn.addEventListener("click", () => {
+  location.reload();
+});
