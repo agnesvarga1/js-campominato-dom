afbcb5cbc090450ffc9cd1808f1409913931673d
agnesvarga1 <agikolyok@gmail.com>
2024-01-18 13:39:43 +0100

    feat html score; fix js

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
+"use strict";
 const mainHtml = document.querySelector("main");
 const gridHtml = document.querySelector("#grid");
 const playBtn = document.querySelector(".play-btn");
 const rstBtn = document.querySelector(".rst-btn");
 const selectHtml = document.querySelector("#diff-level");
-
+const scoreDisplay = document.querySelector("#score-display");
 const bombList = [];
 const totBombs = 16;
 
@@ -30,21 +31,24 @@ function startGame(n) {
     cellHtml.style.setProperty("width", wCalced);
     cellHtml.classList.add("cell");
     cellHtml.innerHTML = `<span>${i}</span>`;
-
+    if (bombList.includes(i)) {
+      cellHtml.classList.add("bomb");
+    }
     cellHtml.addEventListener("click", function () {
       let cellNum = this.querySelector("span").innerText;
 
       let state = bombList.includes(Number(cellNum));
 
       if (state === true) {
-        this.classList.add("bomb");
-        endGame();
+        this.classList.add("bomb-clicked");
+        endGame("GAME OVER");
       } else {
         this.classList.add("active");
+        this.style.setProperty("pointer-events", "none");
         count = count + 1;
-
+        scoreDisplay.innerText = count;
         if (count == cellPerLine ** 2 - totBombs) {
-          winner();
+          endGame("You Won");
         }
       }
     });
@@ -62,20 +66,14 @@ playBtn.addEventListener("click", () => {
   console.log(bombList);
   startGame(diffLevel);
 });
-function endGame() {
+function endGame(msg) {
   const endGameInfos = document.createElement("div");
   endGameInfos.classList.add("end-game");
-  endGameInfos.innerHTML += `<h2>GAME OVER</h2>`;
+  endGameInfos.innerHTML += `<h2>${msg}</h2>`;
   endGameInfos.innerHTML += `<h2>Your Score : ${count}</h2>`;
   mainHtml.appendChild(endGameInfos);
 }
-function winner() {
-  const endGameInfos = document.createElement("div");
-  endGameInfos.classList.add("end-game");
-  endGameInfos.innerHTML += `<h2>YOU WON</h2>`;
-  endGameInfos.innerHTML += `<h2>Your Score : ${count}</h2>`;
-  mainHtml.appendChild(endGameInfos);
-}
+
 rstBtn.addEventListener("click", () => {
   location.reload();
 });
